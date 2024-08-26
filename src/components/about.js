129e58f785bfcd91e158a4769c7cc05ce7ea943e
DeeyBeey <dhruv_belai@yahoo.com>
2024-08-26 15:47:42 -0400

    added resume to about section & fixes

--- src/components/about.js
+++ src/components/about.js
@@ -24,6 +24,9 @@ const AboutSection = () => {
                         excellence. A steadfast believer in perseverance and teamwork, 
                         I am always eager to collaborate and connect with like-minded individuals. 
                         Let's connect and create something extraordinary together!</p>
+
+                    <a href="/data/Dhruv_Belai_Resume.pdf" className="resume-button" target="_blank" rel="noopener noreferrer">View My Resume</a>
+                    <p className="witty-line">I'm somewhat of a tailor myself, so feel free to ask for a custom-fitted resume!</p>
                 </div>
                 <div className="image">
                     <img src="/data/my_picture.jpg" alt="Dhruv Belai" />
