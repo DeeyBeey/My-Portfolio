7cafafca28430abb82fe4245b67e916d116264d3
DeeyBeey <dhruv_belai@yahoo.com>
2024-11-01 11:32:35 -0400

    minor fixes

--- src/components/experience.js
+++ src/components/experience.js
@@ -5,38 +5,35 @@ const ExperienceSection = () => {
 
     return (
         <section id="experience">
+              <h2>A Look at My Experience</h2>
 
             <div className="experience-item">
-                <h2>A Look at My Experience</h2>
                 <div className="role-date">
-                    <h3>Software Engineer Intern</h3>
-                    <p>May 2019 - June 2019</p>
+                    <h3>Teaching Assistant | DS 3000: Foundations of Data Science</h3>
+                    <p>September 2024 - Present</p>
                 </div>
-                <h4>Nibodh Educare</h4>
+                <h4>Northeastern University</h4>
                 <ul>
-                    <li>Created user-friendly interfaces using HTML, CSS, React, and NodeJS for a student-parent
-                        web portal. Connecting the front-end and back-end into the live code of the website, ensuring
-                        smooth functionality.</li>
-                    <li>Optimized data pipelines and SQL queries, improving data retrieval speed by 40% and
-                        boosting system responsiveness, enhancing data analytics efficiency and designed dashboards
-                        on Tableau to support informed decision-making.</li>
-                    <li>Led agile development and CI/CD pipelines, using Git for version control, and automating
-                        the software deployment process to improve collaboration and project management.</li>
-                    <li>Managed communication between stakeholders and the development team to ensure smooth deployment
-                        and gather positive user feedback.</li>
+                    <li>Guided students in data cleaning, analysis, and visualization using Python (Pandas, NumPy) for coursework projects. </li>
+                    <li>Assessed machine learning and statistical models, giving feedback to strengthen data-driven problem-solving skills. </li>
                 </ul>
             </div>
 
             <div className="experience-item">
                 <div className="role-date">
-                    <h3>Python Instructor (Volunteer)</h3>
-                    <p>May 2021 - August 2021</p>
+                    <h3>Data Analyst Intern</h3>
+                    <p>May 2019 - June 2019</p>
                 </div>
-                <h4>AEIT Computer Institute</h4>
+                <h4>Nibodh Educare</h4>
                 <ul>
-                    <li>Taught Python programming basics, including OOP and OOD, to a class of 10 undergraduate students.</li>
-                    <li>Developed and provided instructional materials and resources to enhance student learning and engagement.</li>
-                    <li>Graded student homework and served as a mentor, providing guidance and support to enhance their learning experience.</li>
+                    <li>Led comprehensive data processing, and analytical modelling, transforming raw data into structured formats using 
+                    Python's NumPy library, identifying key insights and presented findings resulting in a 10% enhancement in KPIs. </li>
+                    <li>Leveraged Microsoft Azure cloud services to optimize data storage and ELT data pipelines, resulting in a 30% 
+                    reduction in infrastructure costs while maintaining high scalability and reliability for the data analytics platform. </li>
+                    <li>Designed dynamic dashboards in Tableau, using Git for version control, to effectively communicate key trends, 
+                    facilitating data-driven decision-making and driving measurable improvements in business performance. </li>
+                    <li>Communicated findings and recommendations to stakeholders in a clear and concise manner, fostering understanding 
+                    and enabling informed decision-making processes across departments within a collaborative Scrum team environment. </li>
                 </ul>
             </div>
         </section>
