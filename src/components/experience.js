ce5ff45a3de92304d75867833ff6251712bff30e
DeeyBeey <dhruv_belai@yahoo.com>
2025-01-15 16:26:22 -0500

    minor updates

--- src/components/experience.js
+++ src/components/experience.js
@@ -14,27 +14,27 @@ const ExperienceSection = () => {
                 </div>
                 <h4>Northeastern University</h4>
                 <ul>
-                    <li>Guided students in data cleaning, analysis, and visualization using Python (Pandas, NumPy) for coursework projects. </li>
-                    <li>Introduced data preprocessing, feature engineering, and model evaluation best practices to improve project outcomes. </li>
-                    <li>Assessed machine learning and statistical models, giving feedback to strengthen data-driven problem-solving skills. </li>
+                    <li>Guided students in data cleaning, analysis, and visualization using Python (Pandas, NumPy) for coursework projects.</li>
+                    <li>Reviewed machine learning models, providing actionable feedback to enhance students’ problem-solving capabilities.</li>
+                    <li>Supported 50+ students with precise guidance, achieving only 2 regrade requests alongside 14 TAs and a professor.</li>
                 </ul>
             </div>
 
             <div className="experience-item">
                 <div className="role-date">
                     <h3>Software Engineer Intern</h3>
-                    <p>May 2019 - June 2019</p>
+                    <p>May 2022 - January 2023</p>
                 </div>
                 <h4>Nibodh Educare</h4>
                 <ul>
                     <li>Leveraged HTML, CSS, and JavaScript (MongoDB, Express, React, Node.js) to develop a web application portal, 
-                    integrating front-end and back-end components into the live code of the website, implementing OOD/OOP principles. </li>
-                    <li>Implemented AWS cloud and microservices architecture to optimize data retrieval speeds and develop REST APIs, 
-                    resulting in a 30% increase in system responsiveness, enhancing data analytics for informed decision-making. </li>
-                    <li>Championed agile development methodologies and employed Git for version control, ensuring efficient collaboration 
-                    and project management, including testing using Cypress and Jest, alongside code maintenance. </li>
-                    <li>Facilitated open communication between stakeholders and the development team, ensuring smooth deployment while 
-                    managing expectations and garnering positive user feedback through collaborative problem-solving.</li>
+                    integrating front-end and back-end components into the live code of the website, implementing OOD/OOP principles.</li>
+                    <li>Enhanced system responsiveness by 30% through AWS cloud and microservices architecture, developing RESTful 
+                    APIs, and implementing scalable fault-tolerant systems to ensure high availability and informed decision-making.</li>
+                    <li>Championed agile methodologies and leveraged Git for version control, driving efficient collaboration, project 
+                    management, and comprehensive testing with Cypress and Jest, while ensuring robust code maintenance and delivery.</li>
+                    <li>Streamlined stakeholder-developer communication, ensuring seamless deployments, managing expectations, and 
+                    securing positive user feedback through effective collaboration and problem-solving.</li>
                 </ul>
             </div>
         </section>
