d46102f49971da1c323991357871940de643b485
Dhruv <dhruv_belai@yahoo.com>
2026-03-10 22:58:31 -0400

    Added Lenis for smooth scrolling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import Lenis from 'lenis';
 import Header from './components/header';
 import AboutSection from './components/about'
 import ExperienceSection from './components/experience';
@@ -7,6 +9,28 @@ import Footer from './components/footer';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.1,
+      smoothWheel: true,
+      smoothTouch: false,
+    });
+
+    let frameId;
+
+    const raf = (time) => {
+      lenis.raf(time);
+      frameId = window.requestAnimationFrame(raf);
+    };
+
+    frameId = window.requestAnimationFrame(raf);
+
+    return () => {
+      window.cancelAnimationFrame(frameId);
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <div className="App">
       <Header />
